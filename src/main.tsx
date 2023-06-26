667d9f102f7f0d71ecd69e072bb5a7f611a39477
Alexandr Dylevich <dylevich220885@gmail.com>
2023-06-26 21:47:59 +0300

    init commit

---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import { App } from './App'
+import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
+
+// import * as React from 'react'
+//
+// import * as ReactDOM from 'react-dom/client'
+//
+// import App from './App.tsx'
+// import './index.css'
+//
+// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
