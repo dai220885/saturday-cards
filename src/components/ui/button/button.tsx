ef03e098eb7695d846293c7c8579afedc154b313
Alexandr Dylevich <dylevich220885@gmail.com>
2023-06-27 23:14:33 +0300

    добавил комменты (21. saturday lesson 1 )

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -9,7 +9,10 @@ export type ButtonProps<T extends ElementType = 'button'> = {
   className?: string
 } & ComponentPropsWithoutRef<T>
 
+//ElementType - специальный тип из реакта, это, можно сказать тег (jsx элемент)
 export const Button = <T extends ElementType = 'button'>(
+  //уберем из дефолтных пропсов переданного компонента те, которые будут называться, как наши, а  останутся только те, которые в наших пропсах (в нашем случае className)
+  //TODO мне кажется, что типизацию с Omit-keyof нужно закинуть в тип ButtonProps и тогда здесь можно будет просто указать props: ButtonProps<T> ????
   props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>
 ) => {
   const { variant = 'primary', fullWidth, className, as: Component = 'button', ...rest } = props
