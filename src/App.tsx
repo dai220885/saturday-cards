79069d749db0a60c805fe69787a8de1c69087f73
Alexandr Dylevich <dylevich220885@gmail.com>
2023-06-27 23:13:09 +0300

    добавил тестовую отрисовку кнопки (21. saturday lesson 1 )

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,22 @@
+//import { Button } from './components/ui/button'
+
+import { Button } from './components/ui/button/index'
+import { Link } from 'react-router-dom'
+
 export function App() {
-  return <div>Hello</div>
+  return (
+    <div>
+      <div>Hello</div>
+
+      <Button variant="primary"> Primary Button</Button>
+      <Button variant="primary" as="a" href="https://google.com">
+        Primary Button
+      </Button>
+      <Button variant="secondary" as={Link} to={'https://google.com'}>
+        {' '}
+        Secondary Button
+      </Button>
+      <Link to={'https://google.com'}>LINK</Link>
+    </div>
+  )
 }
