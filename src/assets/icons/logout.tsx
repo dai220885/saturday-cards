acf58c64f7907b49b82dbf1644b00cd3d743ed50
Alexandr Dylevich <dylevich220885@gmail.com>
2023-06-27 23:23:14 +0300

    import * as React from 'react' (21. saturday lesson 1 )

--- src/assets/icons/logout.tsx
+++ src/assets/icons/logout.tsx
@@ -1,4 +1,4 @@
-//import * as React from 'react'
+import * as React from 'react'
 //import React from 'react'
 import { SVGProps, Ref, forwardRef, memo } from 'react'
 const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
